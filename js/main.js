a474ab3d68080cd93129bc908f13146723112632
Sergey <s.yanacov@gmail.com>
2020-03-21 11:26:35 +0200

    - partially fixed task 2 of module 4

--- js/main.js
+++ js/main.js
@@ -8,6 +8,193 @@
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
+  var pinButton = document.querySelector('.map__pin--main');
+  var AdvertisementForm = function () {
+    var formElement = document.querySelector('.ad-form');
+
+    this.submitButton = formElement.querySelector('.ad-form__submit');
+    this.resetButton = formElement.querySelector('.ad-form__reset');
+    this.isDisabled = formElement.classList.contains('ad-form--disabled');
+    this.title = formElement.querySelector('#title');
+    this.address = formElement.querySelector('#address');
+    this.type = formElement.querySelector('#type');
+    this.price = formElement.querySelector('#price');
+    this.timein = formElement.querySelector('#timein');
+    this.timeout = formElement.querySelector('#timeout');
+    this.roomNumber = formElement.querySelector('#room_number');
+    this.capacity = formElement.querySelector('#capacity');
+    this.description = formElement.querySelector('#description');
+    this.images = formElement.querySelector('#images');
+    this.features = {
+      'wifi': formElement.querySelector('#feature-wifi'),
+      'dishwasher': formElement.querySelector('#feature-dishwasher'),
+      'parking': formElement.querySelector('#feature-parking'),
+      'washer': formElement.querySelector('#feature-washer'),
+      'elevator': formElement.querySelector('#feature-elevator'),
+      'conditioner': formElement.querySelector('#feature-conditioner')
+    };
+
+    this.onSubmit = function (handler) {
+      formElement.addEventListener('submit', handler);
+    };
+
+    this.disable = function (isDisabled) {
+      var shouldBeDisabled = typeof isDisabled === 'boolean' ? isDisabled : true;
+
+      if (shouldBeDisabled) {
+        if (!this.isDisabled) {
+          formElement.classList.add('ad-form--disabled');
+        }
+      } else {
+        formElement.classList.remove('ad-form--disabled');
+      }
+
+      this.title.disabled = shouldBeDisabled;
+      this.address.disabled = shouldBeDisabled;
+      this.type.disabled = shouldBeDisabled;
+      this.price.disabled = shouldBeDisabled;
+      this.timein.disabled = shouldBeDisabled;
+      this.timeout.disabled = shouldBeDisabled;
+      this.roomNumber.disabled = shouldBeDisabled;
+      this.capacity.disabled = shouldBeDisabled;
+      this.description.disabled = shouldBeDisabled;
+      this.images.disabled = shouldBeDisabled;
+      this.features.wifi.disabled = shouldBeDisabled;
+      this.features.dishwasher.disabled = shouldBeDisabled;
+      this.features.parking.disabled = shouldBeDisabled;
+      this.features.washer.disabled = shouldBeDisabled;
+      this.features.elevator.disabled = shouldBeDisabled;
+      this.features.conditioner.disabled = shouldBeDisabled;
+      this.submitButton.disabled = shouldBeDisabled;
+      this.resetButton.disabled = shouldBeDisabled;
+    };
+
+    this.areRoomWidthCapacityValid = function () {
+      var result = false;
+      if (this.roomNumber.value === '100' || this.capacity.value === '0') {
+        result = this.capacity.value === '0' && this.roomNumber.value === '100';
+      } else {
+        result = this.roomNumber.value >= this.capacity.value;
+      }
+      return result;
+    };
+
+    this.isRoomNumberValid = function () {
+      var result = false;
+      switch (this.capacity.value) {
+        case '0':
+          result = this.roomNumber.value === '100';
+          break;
+        default:
+          result = this.roomNumber.value >= this.capacity.value;
+          break;
+      }
+      return result;
+    };
+
+    this.isCapacityValid = function () {
+      var result = false;
+      switch (this.roomNumber.value) {
+        case '100':
+          result = this.capacity.value === '0';
+          break;
+        default:
+          result = this.capacity.value <= this.roomNumber.value;
+          break;
+      }
+      return result;
+    };
+  };
+
+  var FilterForm = function () {
+    var formElement = document.querySelector('.map__filters');
+
+    this.type = formElement.querySelector('#housing-type');
+    this.price = formElement.querySelector('#housing-price');
+    this.roomNumber = formElement.querySelector('#housing-rooms');
+    this.capacity = formElement.querySelector('#housing-guests');
+    this.features = {
+      'wifi': formElement.querySelector('#filter-wifi'),
+      'dishwasher': formElement.querySelector('#filter-dishwasher'),
+      'parking': formElement.querySelector('#filter-parking'),
+      'washer': formElement.querySelector('#filter-washer'),
+      'elevator': formElement.querySelector('#filter-elevator'),
+      'conditioner': formElement.querySelector('#filter-conditioner')
+    };
+
+    this.disable = function (isDisabled) {
+      var shouldBeDisabled = typeof isDisabled === 'boolean' ? isDisabled : true;
+
+      this.type.disabled = shouldBeDisabled;
+      this.price.disabled = shouldBeDisabled;
+      this.roomNumber.disabled = shouldBeDisabled;
+      this.capacity.disabled = shouldBeDisabled;
+      this.features.wifi.disabled = shouldBeDisabled;
+      this.features.dishwasher.disabled = shouldBeDisabled;
+      this.features.parking.disabled = shouldBeDisabled;
+      this.features.washer.disabled = shouldBeDisabled;
+      this.features.elevator.disabled = shouldBeDisabled;
+      this.features.conditioner.disabled = shouldBeDisabled;
+    };
+  };
+  var newAdvertisement = new AdvertisementForm();
+  var newFilter = new FilterForm();
+
+  var disablePage = function () {
+    newAdvertisement.disable();
+    newFilter.disable();
+  };
+
+  var enablePage = function () {
+    newAdvertisement.disable(false);
+    newFilter.disable(false);
+  };
+
+  var onPinButtonClick = function (evt) {
+    var mainButton = 0;
+
+    if (evt.button === mainButton) {
+      enablePage();
+    }
+  };
+
+  var onPinButtonEnter = function (evt) {
+    if (evt.key === 'Enter') {
+      enablePage();
+    }
+  };
+
+  var onPinButtonMove = function (evt) {
+    var mainButton = 0;
+    // 44px + 22px - pins height
+
+    if (evt.button === mainButton) {
+      // debugger;
+    }
+  };
+
+  var onRoomNumberSubmit = function () {
+    if (!newAdvertisement.isRoomNumberValid()) {
+      newAdvertisement.roomNumber.setCustomValidity('Количество комнат не может быть меньше количества мест');
+    } else {
+      newAdvertisement.roomNumber.setCustomValidity('');
+    }
+  };
+
+  var onCapacitySubmit = function () {
+    if (!newAdvertisement.isCapacityValid()) {
+      newAdvertisement.capacity.setCustomValidity('Количество мест не может превышать количество комнат');
+    } else {
+      newAdvertisement.capacity.setCustomValidity('');
+    }
+  };
+
+  pinButton.addEventListener('keydown', onPinButtonEnter);
+  pinButton.addEventListener('mousedown', onPinButtonClick);
+  pinButton.addEventListener('mousedown', onPinButtonMove);
+  newAdvertisement.onSubmit(onRoomNumberSubmit);
+  newAdvertisement.onSubmit(onCapacitySubmit);
+
   var getSimilarAdvertisements = function () {
     var PHOTO_SOURCES = [
       'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
@@ -137,8 +324,8 @@
     mapContainer.insertBefore(advertisementCardsFragment, filtersContainer);
   };
 
+  disablePage();
   generatePins();
   generateAdvertisementCards();
-
   document.querySelector('.map').classList.remove('map--faded');
 })();
